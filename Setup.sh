74390c29748eca4d706564ba743c91350e7f6101
t-na <nakamura@h91.155.228.10.30832.vlan.kuins.net>
2015-06-10 18:01:34 +0900

    some fixed

--- Setup.sh
+++ Setup.sh
@@ -4,31 +4,37 @@
 
 # 隠しファイルを表示
 defaults write com.apple.finder AppleShowAllFiles -bool yes
-# クイックルック内のテキストをコピー可能にする
-defaults write com.apple.finder QLEnableTextSelection -bool yes
 #Finder上部にパスを表示
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool yes
-# ネットワーク接続時に.DS_Store ファイルの作成を抑制
-defaults write com.apple.desktopservices DSDontWriteNetworkStores true
-
+# クイックルック内のテキストをコピー可能にする
+defaults write com.apple.finder QLEnableTextSelection -bool yes
+# killall
 killall Finder
 
 
-
-
 ### Dock ###
-
+# 表示されるまでの時間を短くする
+defaults write com.apple.dock autohide-delay -float 0
+# 表示アニメーションを高速化
 defaults write com.apple.dock autohide-time-modifier -float 0.4
-
+# killall
 killall Dock
 
 
-
-
 ### SystemUIServer ###
-# スクリーンショットのタイトルを変更
-defaults write com.apple.screencapture name “screenshot”
-# スクリーンショットの影を無くす
-defaults write com.apple.screencapture “disable-shadow” -bool yes
-
+# スクリーンショットの保存場所を指定
+defaults write com.apple.screencapture location ~/Pictures/Screenshots
+# スクリーンショットのファイル名を指定
+defaults write com.apple.screencapture name screenshot
+# スクリーンショットを影なしで撮影
+defaults write com.apple.screencapture disable-shadow -bool yes
+# killall
 killall SystemUIServer
+
+
+### その他 ###
+# ネットワーク接続時に.DS_Store ファイルの作成を抑制
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool yes
+# killall
+killall Dock
+
