64452f5ec2eb7daaaceb976283b15e7594bf18dc
t-na <admin@Nakamura-Macbook.local>
2015-06-19 05:56:12 +0900

    create .localized

---
+++ Create.localized.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+touch ~/Applications/.localized
+touch ~/Desktop/.localized
+touch ~/Documents/.localized
+touch ~/Downloads/.localized
+touch ~/Library/.localized
+touch ~/Movies/.localized
+touch ~/Music/.localized
+touch ~/Pictures/.localized
+touch ~/Public/.localized
---
+++ Delete.localized.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+rm ~/Applications/.localized
+rm ~/Desktop/.localized
+rm ~/Documents/.localized
+rm ~/Downloads/.localized
+rm ~/Library/.localized
+rm ~/Movies/.localized
+rm ~/Music/.localized
+rm ~/Pictures/.localized
+rm ~/Public/.localized
