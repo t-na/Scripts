c1cd4678bfdbbc899670fab6fa462a7235351fb1
t-na <12282676+t-na@users.noreply.github.com>
2023-09-06 21:50:27 +0900

    fixed a format of each command

--- Setup.sh
+++ Setup.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
 
 ### Finder ###
-
 # 隠しファイルを表示
-defaults write com.apple.finder AppleShowAllFiles -bool yes
+defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
 #Finder上部にパスを表示
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool yes
+defaults write com.apple.finder "_FXShowPosixPathInTitle" -bool "true"
 # クイックルック内のテキストをコピー可能にする
-defaults write com.apple.finder QLEnableTextSelection -bool yes
-# killall
-killall Finder
-
+defaults write com.apple.finder "QLEnableTextSelection" -bool "true"
 
 ### Dock ###
 # 表示されるまでの時間を短くする
-defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock "autohide-delay" -float "0"
 # 表示アニメーションを高速化
-defaults write com.apple.dock autohide-time-modifier -float 0.4
-# killall
-killall Dock
-
+defaults write com.apple.dock "autohide-time-modifier" -float "0.4"
 
 ### SystemUIServer ###
 # スクリーンショットの保存場所を指定
-defaults write com.apple.screencapture location ~/Pictures/Screenshots
+defaults write com.apple.screencapture "location" -string "~/Pictures/Screenshots"
 # スクリーンショットのファイル名を指定
-defaults write com.apple.screencapture name screenshot
+defaults write com.apple.screencapture "name" -string "screenshot"
 # スクリーンショットを影なしで撮影
-defaults write com.apple.screencapture disable-shadow -bool yes
+defaults write com.apple.screencapture "disable-shadow" -bool "true"
 # killall
 killall SystemUIServer
 
-
 ### その他 ###
 # ネットワーク接続時に.DS_Store ファイルの作成を抑制
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool yes
-# killall
-killall Dock
+defaults write com.apple.desktopservices "DSDontWriteNetworkStores" -bool "true"
 
+### killall ###
+killall Finder
+killall Dock
+killall SystemUIServer
+
+# https://macos-defaults.com/
